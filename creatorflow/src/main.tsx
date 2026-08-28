7a56713ad2cdbb82a825e8edfb1a9639200241f5
Carl Laliberté <54148864+carllaliberte@users.noreply.github.com>
2026-08-28 08:53:43 -0400

    fix: drop service workers so Safari cannot freeze an old Clapshot

--- creatorflow/src/main.tsx
+++ creatorflow/src/main.tsx
@@ -6,6 +6,18 @@ import "./index.css";
 
 void initNativeShell();
 
+if ("serviceWorker" in navigator) {
+  void navigator.serviceWorker.getRegistrations().then((regs) => {
+    for (const reg of regs) void reg.unregister();
+  });
+}
+
+if ("caches" in window) {
+  void caches.keys().then((keys) => {
+    for (const key of keys) void caches.delete(key);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
